42c6471d7cc2951456382be65dd2acd427ce8d08
GustavoMSilva <macedo.gustavo@ges.inatel.br>
2026-08-27 15:47:06 -0300

    feat: Implementado funcao simples para salvar uma string no banco de dados (sqlite), essa string representaria o chatID do Telegram do usuario
    Para acessar os endpoints use o caminho /docs
    
    Endpoint /string: Adiciona um ID
    
    Endpoint /strings: Lista os IDs

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 import os
 import uvicorn
+import sqlite3
 from fastapi import FastAPI
 from supabase import create_client, Client
 from dotenv import load_dotenv
@@ -16,6 +17,58 @@ if url and key:
 app = FastAPI()
 
 app.include_router(auth_router)
+
+
+#Cria o banco de dados
+def init_db():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS chatID(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            chat_id TEXT NOT NULL
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+init_db()
+
+@app.post("/string")
+def save_string(chat_id: str):
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO chatID (chat_id) VALUES (?)",
+        (chat_id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return {
+        "message": "Chat ID salvo com sucesso!",
+        "chatID": chat_id
+    }
+@app.get("/strings")
+def get_strings():
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT id, chat_id FROM chatID")
+    strings = cursor.fetchall()
+
+    conn.close()
+
+    return [
+        {
+            "id": row[0],
+            "chat_id": row[1]
+        }
+        for row in strings
+    ]
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
