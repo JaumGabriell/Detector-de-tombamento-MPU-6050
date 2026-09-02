37a6f4d17beeca9cbb1e113768c3e0d5722729ae
Guilherme-O-Brito <gui_o_brito@yahoo.com.br>
2026-09-02 18:10:14 -0300

    delete: supabase suport

--- backend/main.py
+++ backend/main.py
@@ -2,16 +2,10 @@ import os
 import uvicorn
 import sqlite3
 from fastapi import FastAPI
-from supabase import create_client, Client
 from dotenv import load_dotenv
 from routes.auth_routes import auth_router
 
 load_dotenv()
-
-url: str = os.getenv("SUPABASE_URL")
-key: str = os.getenv("SUPABASE_KEY")
-if url and key:
-    supabase: Client = create_client(url, key)
 
 # comando pra rodar: uvicorn main:app --reload
 app = FastAPI()
